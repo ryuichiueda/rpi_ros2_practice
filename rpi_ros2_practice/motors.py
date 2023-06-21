78484e481725786dafb1d09305fb017c84fe3666
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-06-21 04:16:48 +0000

    Add motor node

---
+++ rpi_ros2_practice/motors.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2023 Ryuichi Ueda 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+import rclpy, math
+from rclpy.node import Node
+from std_msgs.msg import Int16MultiArray
+
+class Motors():
+    def __init__(self, node_ref):
+        self.node = node_ref
+
+        self.sub_freqs = self.node.create_subscription(Int16MultiArray, 'motor_raw', self.callback_motor_raw, 10)
+
+
+    def callback_motor_raw(self, msg):
+        lfile = '/dev/rtmotor_raw_l0'
+        rfile = '/dev/rtmotor_raw_r0'
+        
+        try:
+            lf = open(lfile,'w')
+            rf = open(rfile,'w')
+            lf.write("%s\n" % msg.data[0])
+            rf.write("%s\n" % msg.data[1])
+        except:
+            self.node.get_logger().info("cannot write to rtmotor_raw_*")
+    
+        lf.close()
+        rf.close()
+
+
+def main():
+    rclpy.init()
+    node = Node("motors")
+    talker = Motors(node)
+    rclpy.spin(node)
+
+
+if __name__ == '__main__':
+    main()
