9c8c2288c083449670e1e91e47d920730723383f
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-06-21 05:17:49 +0000

    Add launch files

---
+++ launch/run.launch.py
@@ -0,0 +1,18 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    lightsensors = launch_ros.actions.Node(
+        package='rpi_ros2_practice',
+        executable='lightsensors',
+        output='screen',
+        )
+    motors = launch_ros.actions.Node(
+        package='rpi_ros2_practice',
+        executable='motors',
+        output='screen',
+        )
+
+    return launch.LaunchDescription([lightsensors, motors])
