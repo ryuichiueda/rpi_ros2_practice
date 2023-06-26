86206a967b34e3a52055d3fbc3692179f9b7435a
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-06-26 08:14:24 +0000

    Use msg file

--- rpi_ros2_practice/motors.py
+++ rpi_ros2_practice/motors.py
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy, math
 from rclpy.node import Node
-from std_msgs.msg import Int16MultiArray
+from rpi_ros2_practice_msgs.msg import MotorFreqs
 
 class Motors(Node):
     def __init__(self):
         super().__init__("motors")
-        self.sub_freqs = self.create_subscription(Int16MultiArray, 'motor_raw', self.callback_motor_raw, 10)
+        self.sub_freqs = self.create_subscription(MotorFreqs, 'motor_raw', self.callback_motor_raw, 10)
 
 
     def callback_motor_raw(self, msg):
@@ -19,8 +19,8 @@ class Motors(Node):
             lf = open(lfile,'w')
             rf = open(rfile,'w')
             #self.node.get_logger().info("send {}".format(msg.data[0]))
-            lf.write("%s\n" % msg.data[0])
-            rf.write("%s\n" % msg.data[1])
+            lf.write("%s\n" % msg.left)
+            rf.write("%s\n" % msg.right)
             lf.flush()
             rf.flush()
         except:
