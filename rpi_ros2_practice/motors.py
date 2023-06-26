dc41b503da3ad273c0cd24f270a2911fd4aa5a45
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-06-26 08:29:51 +0000

    Fix

--- rpi_ros2_practice/motors.py
+++ rpi_ros2_practice/motors.py
@@ -3,22 +3,41 @@
 import rclpy, math
 from rclpy.node import Node
 from rpi_ros2_practice_msgs.msg import MotorFreqs
+from rpi_ros2_practice_msgs.srv import SwitchMotors
 
 class Motors(Node):
     def __init__(self):
         super().__init__("motors")
         self.sub_freqs = self.create_subscription(MotorFreqs, 'motor_raw', self.callback_motor_raw, 10)
+        self.srv_motor_power = self.create_service(SwitchMotors, 'switch_motors', self.callback_motor_sw)
+
+
+    def callback_motor_sw(self, request, response):
+        enfile = '/dev/rtmotoren0'
+        try:
+            with open(enfile,'w') as f:
+                if request.command == "on":    f.write("1\n")
+                elif request.command == "off": f.write("0\n")
+                else: 
+                    self.get_logger().info("invalid request")
+                    response.result = "NG"
+                    return response    
+
+            response.result = "OK"
+        except:
+            self.get_logger().info("cannot write to " + enfile)
+            response.result = "NG"
+
+        return response    
 
 
     def callback_motor_raw(self, msg):
-        #self.node.get_logger().info("send {}".format(msg))
         lfile = '/dev/rtmotor_raw_l0'
         rfile = '/dev/rtmotor_raw_r0'
         
         try:
             lf = open(lfile,'w')
             rf = open(rfile,'w')
-            #self.node.get_logger().info("send {}".format(msg.data[0]))
             lf.write("%s\n" % msg.left)
             rf.write("%s\n" % msg.right)
             lf.flush()
